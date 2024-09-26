90da8f0ef009e4b7e8daf460edc4a0c746f21351
Gabriel Ferreira <72238863+Howzy11@users.noreply.github.com>
2024-09-26 23:50:52 +0000

    add new code

--- novos-codigos/ex01.py
+++ novos-codigos/ex01.py
@@ -1,1 +1,6 @@
 # Neste algoritmo, crie uma variável que armazene uma string e uma lista que armazena várias strings.
+valor1 = input("Insira seu nome: ")
+array = ["Estudar na DIO", "Candidatar a vagas", "Descansar"]
+
+print(valor1)
+print (array)
